cc0162bef150339427f59ea6e9acda51cb3bfdc6
Gabriel <gabriel.medeiros.2002@hotmail.com>
2023-04-13 10:27:14 -0300

    POO Introduction Rv3

---
+++ IntroductionPOO/src/main/java/ex/CurrencyConverter.java
@@ -0,0 +1,8 @@
+package ex;
+
+public class CurrencyConverter {
+
+    public static double calcDollar(double price, double dollarNeed){
+        return price * dollarNeed * 1.06;
+    }
+}
---
+++ IntroductionPOO/src/main/java/ex/Program.java
@@ -0,0 +1,20 @@
+package ex;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("What is the dollar price?");
+        double dollarPrice = sc.nextDouble();
+
+        System.out.print("How many dollars will be bought?");
+        double dollarNeed = sc.nextDouble();
+
+        System.out.printf("Amount to be paid in reais = %.2f%n", CurrencyConverter.calcDollar(dollarPrice, dollarNeed));
+    }
+}
